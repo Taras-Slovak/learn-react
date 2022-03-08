ddb1d5c83f2258e948329653368e97391829069a
Taras Slovak <slovak.taras@gmail.com>
2022-03-08 17:21:27 +0200

    added event and chanhing state

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ class App extends Component {
 		pageTitle: 'React components'
 	};
 
+	changeTitleHandler = (newTitle) => {
+		this.setState({
+			pageTitle: newTitle
+		});
+	};
+
 	render() {
 		const divStyle = {
 			textAlign: 'center'
@@ -23,9 +29,11 @@ class App extends Component {
 			<div style={divStyle}>
 				<h1>{this.state.pageTitle}</h1>
 
-				<Car name={cars[0].name} year={cars[0].year} />
-				<Car name={cars[1].name} year={cars[1].year} />
-				<Car name={cars[2].name} year={cars[2].year} />
+				<button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button>
+
+				<Car name={cars[0].name} year={cars[0].year} onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)} />
+				<Car name={cars[1].name} year={cars[1].year} onChangeTitle={() => this.changeTitleHandler(cars[1].name)} />
+				<Car name={cars[2].name} year={cars[2].year} onChangeTitle={() => this.changeTitleHandler(cars[2].name)} />
 			</div>
 		);
 	}
