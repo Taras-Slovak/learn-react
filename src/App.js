be4808ef11c1d87780e70b0eb68d2d1d8d6b5529
Taras Slovak <slovak.taras@gmail.com>
2022-03-12 18:23:30 +0200

    added Counter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import Car from './Car/Car';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Counter from './Counter/Counter';
 
 class App extends Component {
 	constructor(props) {
@@ -9,9 +10,9 @@ class App extends Component {
 
 		this.state = {
 			cars: [
-				{ name: 'Ford', year: 2018 },
-				{ name: 'Audi', year: 2016 },
-				{ name: 'Mazda', year: 2010 }
+				{ name: 'Ford', year: 2018 }
+				// {name: 'Audi', year: 2016},
+				// {name: 'Mazda', year: 2010}
 			],
 			pageTitle: 'React components',
 			showCars: false
@@ -75,7 +76,10 @@ class App extends Component {
 				{/*<h1>{this.state.pageTitle}</h1>*/}
 				<h1>{this.props.title}</h1>
 
-				<button className={'AppButton'} onClick={this.toggleCarsHandler}>
+				<Counter />
+
+				<hr />
+				<button style={{ marginTop: 20 }} className={'AppButton'} onClick={this.toggleCarsHandler}>
 					Toggle cars
 				</button>
 
