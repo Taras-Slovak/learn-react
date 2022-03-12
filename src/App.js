9c68681e68f760ad730ecb9137dd47db613c6758
Taras Slovak <slovak.taras@gmail.com>
2022-03-12 17:48:43 +0200

    added ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Car from './Car/Car';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 	constructor(props) {
-		console.log('App constructor');
 		super(props);
 
 		this.state = {
@@ -58,13 +58,14 @@ class App extends Component {
 		if (this.state.showCars) {
 			cars = this.state.cars.map((car, index) => {
 				return (
-					<Car
-						key={index}
-						name={car.name}
-						year={car.year}
-						onDelete={this.deleteHandler.bind(this, index)}
-						onChangeName={(event) => this.onChangeName(event.target.value, index)}
-					/>
+					<ErrorBoundary key={index}>
+						<Car
+							name={car.name}
+							year={car.year}
+							onDelete={this.deleteHandler.bind(this, index)}
+							onChangeName={(event) => this.onChangeName(event.target.value, index)}
+						/>
+					</ErrorBoundary>
 				);
 			});
 		}
