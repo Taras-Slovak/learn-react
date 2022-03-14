6278059ce0d3e868a49414e62f0abeaf08b41c08
Taras Slovak <slovak.taras@gmail.com>
2022-03-14 16:42:19 +0200

    added click event

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -2,18 +2,21 @@ import React from 'react';
 import classes from './ActiveQuiz.module.scss';
 import AnswersList from './AnswersList/AnswersList';
 
-const ActiveQuiz = (props) => (
-	<div className={classes.ActiveQuiz}>
-		<p className={classes.Question}>
-			<span>
-				<strong>2.</strong>&nbsp; How are you?
-			</span>
+const ActiveQuiz = (props) => {
+	return (
+		<div className={classes.ActiveQuiz}>
+			<p className={classes.Question}>
+				<span>
+					<strong>2.</strong>&nbsp;
+					{props.question}
+				</span>
 
-			<small>4 from 12</small>
-		</p>
+				<small>4 из 12</small>
+			</p>
 
-		<AnswersList answers={props.answers} />
-	</div>
-);
+			<AnswersList answers={props.answers} onAnswerClick={props.onAnswerClick} />
+		</div>
+	);
+};
 
 export default ActiveQuiz;
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,9 +6,20 @@ class Quiz extends Component {
 	state = {
 		quiz: [
 			{
-				answers: [{ text: 'Question 1' }, { text: 'Question 2' }, { text: 'Question 3' }, { text: 'Question 4' }]
+				question: 'Какого цвета небо?',
+				rightAnswerId: 2,
+				answers: [
+					{ text: 'Black', id: 1 },
+					{ text: 'Blue', id: 2 },
+					{ text: 'Red', id: 3 },
+					{ text: 'Green', id: 4 }
+				]
 			}
 		]
+	};
+
+	onAnswerClickHandler = (answerId) => {
+		console.log('Answer Id: ', answerId);
 	};
 
 	render() {
@@ -17,7 +28,11 @@ class Quiz extends Component {
 				<div className={classes.QuizWrapper}>
 					<h1>Answer all the questions</h1>
 
-					<ActiveQuiz answers={this.state.quiz[0].answers} />
+					<ActiveQuiz
+						answers={this.state.quiz[0].answers}
+						question={this.state.quiz[0].question}
+						onAnswerClick={this.onAnswerClickHandler}
+					/>
 				</div>
 			</div>
 		);
