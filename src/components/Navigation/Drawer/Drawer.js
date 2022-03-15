725146e15ddb7ed0fe3b98d2b0f625bd4a09eb99
Taras Slovak <slovak.taras@gmail.com>
2022-03-15 14:45:59 +0200

    added Backdrop

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import classes from './Drawer.module.scss';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const links = [1, 2, 3];
-
 class Drawer extends Component {
 	renderLinks() {
 		return links.map((link, index) => {
 			return (
 				<li key={index}>
-					<a>Link {link}</a>
+					<a>Link{link}</a>
 				</li>
 			);
 		});
@@ -22,9 +22,12 @@ class Drawer extends Component {
 		}
 
 		return (
-			<nav className={cls.join(' ')}>
-				<ul>{this.renderLinks()}</ul>
-			</nav>
+			<React.Fragment>
+				<nav className={cls.join(' ')}>
+					<ul>{this.renderLinks()}</ul>
+				</nav>
+				{this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+			</React.Fragment>
 		);
 	}
 }
