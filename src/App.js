71e37499a7a9fb6cbb8980b8e7bf170d967d8c0e
Taras Slovak <slovak.taras@gmail.com>
2022-03-17 15:26:48 +0200

    clear all

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react';
-import './App.scss';
-import About from './About/About';
-import Cars from './Cars/Cars';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<nav className='nav'>
-					<ul>
-						<li>
-							<a href='/'>Home</a>
-						</li>
-						<li>
-							<a href='/about'>About</a>
-						</li>
-					</ul>
-				</nav>
-
-				<hr />
-				<About />
-
-				<Cars />
-			</div>
-		);
-	}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
