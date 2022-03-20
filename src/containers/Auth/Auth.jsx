da1b9f0ab3171e4eb5deae852b4967d06be5ca94
Taras Slovak <slovak.taras@gmail.com>
2022-03-20 20:39:58 +0200

    added Authorization

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,11 +1,36 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import classes from './Auth.module.scss';
+import Button from '../../components/UI/Button/Button';
 
 export default class Auth extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Auth</h1>
-      </div>
-    )
-  }
+	loginHandler = () => {};
+
+	registerHandler = () => {};
+
+	submitHandler = (event) => {
+		event.preventDefault();
+	};
+
+	render() {
+		return (
+			<div className={classes.Auth}>
+				<div>
+					<h1>Authorization</h1>
+
+					<form onSubmit={this.submitHandler} className={classes.AuthForm}>
+						<input type='text' />
+						<input type='text' />
+
+						<Button type='success' onClick={this.loginHandler}>
+							Sign In
+						</Button>
+
+						<Button type='primary' onClick={this.registerHandler}>
+							Sign up
+						</Button>
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
