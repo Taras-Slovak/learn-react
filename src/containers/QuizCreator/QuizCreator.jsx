9edffd35305c1deffa8aa5726904a8176c71f767
Taras Slovak <slovak.taras@gmail.com>
2022-03-23 21:37:25 +0200

    Test creation

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,11 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import classes from './QuizCreator.module.scss';
+import Button from '../../components/UI/Button/Button';
 
 export default class QuizCreator extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Quiz Creator</h1>
-      </div>
-    )
-  }
+	sibmitHandler = (event) => {
+		event.preventDefault();
+	};
+
+	addQuestionHandler = () => {};
+
+	createQuizHandler = () => {};
+
+	render() {
+		return (
+			<div className={classes.QuizCreator}>
+				<div>
+					<h1>Test creation</h1>
+
+					<form onSubmit={this.submitHandler}>
+						<input type='text' />
+						<hr />
+						<input type='text' />
+						<input type='text' />
+						<input type='text' />
+						<input type='text' />
+
+						<select></select>
+
+						<Button type='primary' onClick={this.addQuestionHandler}>
+							Add a question
+						</Button>
+
+						<Button type='success' onClick={this.createQuizHandler}>
+							Create test
+						</Button>
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
