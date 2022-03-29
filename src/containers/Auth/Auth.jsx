acf1e351711781952cd7f5b232f2ccae05d922c3
Taras Slovak <slovak.taras@gmail.com>
2022-03-29 15:23:04 +0300

    added auth

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -3,7 +3,9 @@ import classes from './Auth.module.scss';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import is from 'is_js';
+import axios from 'axios';
 
+const webAPI = 'AIzaSyAQAAI-NRc5PoGHHTRvZJfOtW_4qdJLVSA';
 export default class Auth extends Component {
 	state = {
 		isFormValid: false,
@@ -35,9 +37,40 @@ export default class Auth extends Component {
 		}
 	};
 
-	loginHandler = () => {};
+	loginHandler = async () => {
+		const authData = {
+			email: this.state.formControls.email.value,
+			password: this.state.formControls.password.value,
+			returnSecureToken: true
+		};
+		try {
+			const response = await axios.post(
+				`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${webAPI}`,
+				authData
+			);
 
-	registerHandler = () => {};
+			console.log(response.data);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	registerHandler = async () => {
+		const authData = {
+			email: this.state.formControls.email.value,
+			password: this.state.formControls.password.value,
+			returnSecureToken: true
+		};
+		try {
+			const response = await axios.post(
+				`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${webAPI}`,
+				authData
+			);
+			console.log(response.data);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
 	submitHandler = (event) => {
 		event.preventDefault();
