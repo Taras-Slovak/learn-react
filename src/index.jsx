a9ed8bac730875b2e2a369f87c7d3e76bc26acc8
Taras Slovak <slovak.taras@gmail.com>
2022-03-30 18:35:54 +0300

    added react dev tool

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux/rootReducer';
 import reduxThunk from 'redux-thunk';
+
+const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+				// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+		  })
+		: compose;
 
 const loggerMiddleware = (store) => (next) => (action) => {
 	const result = next(action);
@@ -16,7 +23,7 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
 const store = createStore(
 	rootReducer,
-	applyMiddleware(loggerMiddleware, reduxThunk)
+	composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk))
 );
 
 ReactDOM.render(
