423fa5a78b140b1d34f11f2ab6d18e2982fe7430
Taras Slovak <slovak.taras@gmail.com>
2022-03-30 13:59:14 +0300

    created counter and refactoirn project

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+	state = {
+		counter: 0
+	};
+
+	updateCounter(value) {
+		this.setState({
+			counter: this.state.counter + value
+		});
+	}
+
+	render() {
+		return (
+			<div className={'App'}>
+				<h1>
+					Counter <strong>{this.state.counter}</strong>
+				</h1>
+
+				<hr />
+
+				<div className='Actions'>
+					<button onClick={() => this.updateCounter(1)}>Add 1</button>
+					<button onClick={() => this.updateCounter(-1)}>Subtract 1</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
