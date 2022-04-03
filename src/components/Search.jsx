855efaf76b4f1bd42edc5cb41c903f1e4aadb675
Taras Slovak <slovak.taras@gmail.com>
2022-04-03 15:20:32 +0300

    added code for search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AlertContext } from '../context/alert/alertContext';
 
 export const Search = () => {
+	const [value, setValue] = useState('');
 	const { show } = useContext(AlertContext);
 
 	const onSubmit = (event) => {
-		if (event.key === 'Enter') {
-			show('This is alert!');
+		if (event.key !== 'Enter') {
+			return;
+		}
+
+		if (value.trim()) {
+			console.log('Make request with: ', value);
+		} else {
+			show('Enter User Data!');
 		}
 	};
+
 	return (
-		<div className='row'>
-			<div className='col'>
-				<input
-					type='text'
-					className='form-control'
-					placeholder='Please enter your username...'
-					onKeyPress={onSubmit}
-				/>
-			</div>
+		<div className='form-group'>
+			<input
+				type='text'
+				className='form-control'
+				placeholder='Enter username...'
+				value={value}
+				onChange={(event) => setValue(event.target.value)}
+				onKeyPress={onSubmit}
+			/>
 		</div>
 	);
 };
