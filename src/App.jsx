a6e80f808d935af2c652fe8af228ba507715c048
Taras Slovak <slovak.taras@gmail.com>
2022-04-04 14:26:07 +0300

    created github state

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import { Profile } from './pages/Profile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Alert } from './components/Alert';
 import { AlertState } from './context/alert/AlertState';
+import { GithubState } from './context/github/GithubState';
 
 function App() {
 	return (
-		<AlertState>
-			<BrowserRouter>
-				<Navbar />
-				<div className='container pt-4'>
-					<Alert alert={{ text: 'Test alert' }} />
-					<Routes>
-						<Route path={'/'} element={<Home />}></Route>
-						<Route path={'about'} element={<About />}></Route>
-						<Route path={'profile/:name'} element={<Profile />}></Route>
-					</Routes>
-				</div>
-			</BrowserRouter>
-		</AlertState>
+		<GithubState>
+			<AlertState>
+				<BrowserRouter>
+					<Navbar />
+					<div className='container pt-4'>
+						<Alert alert={{ text: 'Test alert' }} />
+						<Routes>
+							<Route path={'/'} element={<Home />}></Route>
+							<Route path={'about'} element={<About />}></Route>
+							<Route path={'profile/:name'} element={<Profile />}></Route>
+						</Routes>
+					</div>
+				</BrowserRouter>
+			</AlertState>
+		</GithubState>
 	);
 }
 
