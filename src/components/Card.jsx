0c61ed002120962fb9b578d06ce201e02fc4a995
Taras Slovak <slovak.taras@gmail.com>
2022-04-04 18:18:50 +0300

    finished main page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Card = () => {
+export const Card = ({ user }) => {
 	return (
 		<div className='card'>
 			<div className='card-body'>
-				<img src='' className='card-img-top' alt='' />
-				<h5 className='card-title'>React JS</h5>
+				<img src={user.avatar_url} alt={user.login} className='card-img-top' />
+				<h5 className='card-title'>{user.login}</h5>
 				<Link to={`profile/${'react'}`} className='btn btn-primary'>
 					Open
 				</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Search } from '../components/Search';
 import { Card } from '../components/Card';
+import { GithubContext } from '../context/github/githubContext';
 
 export const Home = () => {
-	const cards = new Array(15).fill('').map((_, i) => i);
+	const { loading, users } = useContext(GithubContext);
+
 	return (
-		<>
+		<Fragment>
 			<Search />
+
 			<div className='row pt-4'>
-				{cards.map((card) => {
-					return (
-						<div className='col-sm-4 mb-4' key={card}>
-							<Card />
+				{loading ? (
+					<p className='text-center'>Downloading...</p>
+				) : (
+					users.map((user) => (
+						<div className='col-sm-4 mb-4' key={user.id}>
+							<Card user={user} />
 						</div>
-					);
-				})}
+					))
+				)}
 			</div>
-		</>
+		</Fragment>
 	);
 };
