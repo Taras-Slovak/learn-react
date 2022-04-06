cc68f9cd6df659ccedaabc2167a817143b86d2c2
Taras Slovak <slovak.taras@gmail.com>
2022-04-06 17:52:48 +0300

    added code to Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {
+	Paper,
+	Typography,
+	useMediaQuery
+} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './style';
 
 const Map = () => {
-	return <h1>Map</h1>;
+	const classes = useStyles();
+	const isMobile = useMediaQuery('(min-width:600px)');
+
+	const cordinates = { lat: 0, lng: 0 };
+	return (
+		<div className={classes.mapContainer}>
+			<GoogleMapReact
+				bootstrapURLKeys={{
+					key: 'AIzaSyDmsKNYXvydGXU_bQcJkqKSSjFmbGWT8Z0'
+				}}
+				defaultCenter={cordinates}
+				center={cordinates}
+				defaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={''}
+				onChange={''}
+				onChildClick={''}
+			></GoogleMapReact>
+		</div>
+	);
 };
 
 export default Map;
