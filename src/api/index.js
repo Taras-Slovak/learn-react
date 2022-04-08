3e027bdd0aaba59fa8acdb27e58867881fa32fc0
Taras Slovak <slovak.taras@gmail.com>
2022-04-08 20:57:49 +0300

    added api

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const URL = `https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary`;
+
+const options = {
+	method: 'GET',
+	params: {
+		bl_latitude: '11.847676',
+		tr_latitude: '12.838442',
+		bl_longitude: '109.095887',
+		tr_longitude: '109.149359',
+		restaurant_tagcategory_standalone: '10591',
+		restaurant_tagcategory: '10591',
+		limit: '30',
+		currency: 'USD',
+		open_now: 'false',
+		lunit: 'km',
+		lang: 'en_US'
+	},
+	headers: {
+		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+		'X-RapidAPI-Key':
+			'5c402584c1mshf9ad44bea4d5e27p109868jsn37ce81a5a702'
+	}
+};
+
+export const getPlaceData = async () => {
+	try {
+		const {
+			data: { data }
+		} = await axios.get(URL, options);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
