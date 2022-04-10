25e87bcaa4c66f81cfa0ba1e677566cede05fecb
Taras Slovak <slovak.taras@gmail.com>
2022-04-10 15:44:46 +0300

    added code for PlaceDetails

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import {
+	Box,
+	Typography,
+	Button,
+	Card,
+	CardMedia,
+	CardActions,
+	CardContent,
+	Chip
+} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './style';
 
 const PlaceDetails = ({ place }) => {
-	return <h1>{place.name}</h1>;
+	const classes = useStyles();
+	const imgURL =
+		'https://images.unsplash.com/photo-1576723664541-23f84c3f93fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
+
+	return (
+		<Card elevation={6}>
+			<CardMedia
+				style={{ height: 350 }}
+				image={
+					place.photo
+						? place.photo.images.large.url
+						: imgURL
+				}
+				title={place.name}
+			/>
+			<CardContent>
+				<Typography gutterBottom variant='h5'>
+					{place.name}
+				</Typography>
+				<Box display='flex' justifyContent='space-between'>
+					<Typography variant='subtitle1'>Price</Typography>
+					<Typography gutterBottom variant='subtitle1'>
+						{place.price_level}
+					</Typography>
+				</Box>
+				<Box display='flex' justifyContent='space-between'>
+					<Typography variant='subtitle1'>
+						Ranking
+					</Typography>
+					<Typography gutterBottom variant='subtitle1'>
+						{place.ranking}
+					</Typography>
+					{place?.awards?.map((award) => (
+						<Box
+							my={1}
+							display='flex'
+							justifyContent='space-between'
+							alignItems='center'
+						>
+							<img
+								src={award.image.small}
+								alt={award.display_name}
+							/>
+							<Typography
+								variant='subtitle2'
+								color='textSecondary'
+							>
+								{award.display_name}
+							</Typography>
+						</Box>
+					))}
+				</Box>
+				{place?.cuisine?.map(({ name }) => (
+					<Chip
+						key={name}
+						size='small'
+						label={name}
+						className={classes.chip}
+					/>
+				))}
+			</CardContent>
+		</Card>
+	);
 };
 
 export default PlaceDetails;
