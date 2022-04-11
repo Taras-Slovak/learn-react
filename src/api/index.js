bb831f9007a29c82dd4a461947797fe2397aede4
Taras Slovak <slovak.taras@gmail.com>
2022-04-11 15:58:21 +0300

    reafctoring

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,26 @@
 import axios from 'axios';
 
-const URL = `https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary`;
-
-export const getPlaceData = async (sw, ne) => {
+export const getPlaceData = async (type, sw, ne) => {
 	try {
 		const {
 			data: { data }
-		} = await axios.get(URL, {
-			params: {
-				bl_latitude: sw.lat,
-				tr_latitude: ne.lat,
-				bl_longitude: sw.lng,
-				tr_longitude: ne.lng
-			},
-			headers: {
-				'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-				'X-RapidAPI-Key':
-					'8c3db628d6mshaf7eedccfd349cap1d2fcejsn39db1e5595c5'
+		} = await axios.get(
+			`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+			{
+				params: {
+					bl_latitude: sw.lat,
+					tr_latitude: ne.lat,
+					bl_longitude: sw.lng,
+					tr_longitude: ne.lng
+				},
+				headers: {
+					'X-RapidAPI-Host':
+						'travel-advisor.p.rapidapi.com',
+					'X-RapidAPI-Key':
+						'8c3db628d6mshaf7eedccfd349cap1d2fcejsn39db1e5595c5'
+				}
 			}
-		});
+		);
 		return data;
 	} catch (error) {
 		console.log(error);
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -15,10 +15,16 @@ import {
 import useStyles from './style';
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
-const List = ({ places, childClicked, isLoading }) => {
+const List = ({
+	places,
+	childClicked,
+	isLoading,
+	type,
+	setType,
+	rating,
+	setRating
+}) => {
 	const classes = useStyles();
-	const [type, setType] = useState('restaurants');
-	const [rating, setRating] = useState('');
 	const [elRef, setElRef] = useState([]);
 
 	useEffect(() => {
