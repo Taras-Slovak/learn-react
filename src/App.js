a6176e30f75ecd8d9ca2e3c0d2b422fd52b7e0ba
Taras Slovak <slovak.taras@gmail.com>
2022-04-13 16:33:09 +0300

    added code for App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space, Divider } from 'antd';
+
 function App() {
   return (
-    <h1>TEST</h1>
+    <div className="app">
+      <div className="navbar">Some</div>
+      <div className="main">Some</div>
+      <div className="footer">Some</div>
+    </div>
+
   );
 }
 
