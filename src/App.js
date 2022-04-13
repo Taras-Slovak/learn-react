8ec7f5d4342e10a33626b47bceae59b7f09d9b99
Taras Slovak <slovak.taras@gmail.com>
2022-04-13 17:50:30 +0300

    added code to Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout, Typography, Space, Divider } from 'antd';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="app">
-      <div className="navbar">Some</div>
+      <div className="navbar"><Navbar /></div>
       <div className="main">Some</div>
       <div className="footer">Some</div>
     </div>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined,
+  BulbOutlined, FondOutlined,
+  MenuOutlined } from '@ant-design/icons';
+
+import icon from '../images/cryptocurrency.png';
+
+function Navbar() {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          {/* <Link to="/">Crytoverse</Link> */}
+        </Typography.Title>
+        {/* <Button className="menu-control-container">
+
+        </Button> */}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
