61fdffbd04b29f9b8bc2d66fc1fe26ee3bc386ff
Taras Slovak <slovak.taras@gmail.com>
2022-04-16 17:04:34 +0300

    rendering first data

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -22,11 +22,11 @@ if (isFetching) return 'Loading...';
       >Global Crypto Stats
       </Title>
       <Row>
-        <Col span={12}> <Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}> <Statistic title="Total Exchanges" value="5" /></Col>
-        <Col span={12}> <Statistic title="Total Market Cup" value="5" /></Col>
-        <Col span={12}> <Statistic title="Total 24h Volume" value="5" /></Col>
-        <Col span={12}> <Statistic title="Total Markets" value="5" /></Col>
+        <Col span={12}> <Statistic title="Total Cryptocurrencies" value={millify(globalStats.total)} /></Col>
+        <Col span={12}> <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}> <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+        <Col span={12}> <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+        <Col span={12}> <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
     </>
   );
