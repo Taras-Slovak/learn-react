d36e4fcd4536a2ffad435311c59639ee99733b4f
Taras Slovak <slovak.taras@gmail.com>
2022-04-16 17:56:36 +0300

    added code to Homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '.';
 
 const { Title } = Typography;
 
@@ -28,6 +29,23 @@ if (isFetching) return 'Loading...';
         <Col span={12}> <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}> <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+        <Title level={3} className="show-more">
+          <Link to="cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more">
+          <Link to="news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
+
     </>
   );
 }
