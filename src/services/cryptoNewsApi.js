62be94666fd1e5621b4e90bd1c5ded8a007f6e58
Taras Slovak <slovak.taras@gmail.com>
2022-04-17 15:39:43 +0300

    added cryptoNewsApi

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'X-BingApis-SDK': 'true',
+  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+  'X-RapidAPI-Key': 'fc5b1c4387mshffd01bad35036e4p1db388jsncc024e4ca73e',
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(
+        `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`,
+),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptoNewQuery,
+} = cryptoNewsApi;
+
