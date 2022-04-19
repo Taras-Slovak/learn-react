d5f14e9c5615a54ddfb97477f579bfb78cfd5298
Taras Slovak <slovak.taras@gmail.com>
2022-04-19 17:47:30 +0300

    added loader

--- src/Components/CryptoDetails.jsx
+++ src/Components/CryptoDetails.jsx
@@ -9,6 +9,7 @@ import {
   CheckOutlined, NumberOutlined, ThunderboltOutlined,
 } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+import { Loader } from '.';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -19,8 +20,9 @@ function CryptoDetails() {
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const cryptoDetails = data?.data?.coin;
 
-  
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+  if (isFetching) return <Loader />;
 
   const stats = [
     { title: 'Price to USD',
--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Loader } from '.';
 
 function Cryptocurrencies({ simplified }) {
   const count = simplified ? 10 : 100;
@@ -16,7 +17,7 @@ function Cryptocurrencies({ simplified }) {
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
-  if (isFetching) return 'Loading...';
+  if (isFetching) return <Loader />;
 
   return (
     <>
