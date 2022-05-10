bb9832f1f463f6565d4e1622a2b154f8526be5e7
Taras Slovak <slovak.taras@gmail.com>
2022-05-10 22:25:22 +0300

    added code

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
 export const Banner = ({
   purpose,
   title1,
@@ -37,7 +39,7 @@ export const Banner = ({
   </Flex>
 );
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
     <Box>
       <Banner
@@ -67,4 +69,20 @@ export default function Home() {
       <Flex flexWrap='wrap'>{/* Need to create fetching   */}</Flex>
     </Box>
   );
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`,
+  );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`,
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
