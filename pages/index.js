1bb7dfb9ae8ef56efe8e9d9a0a98034d47ebba46
Taras Slovak <slovak.taras@gmail.com>
2022-05-10 16:32:35 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export const Banner = ({
   imageUrl,
 }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} alt='Image' />
+    <Image src={imageUrl} width={500} height={300} alt='banner' />
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>
         {purpose}
