3044f549fb958fe7f8d3c6e330a93d0c883fbbfe
Taras Slovak <slovak.taras@gmail.com>
2022-05-13 16:22:41 +0300

    change

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_BAYUT_KEY,
+      'x-rapidapi-key': 'fc5b1c4387mshffd01bad35036e4p1db388jsncc024e4ca73e',
     },
   });
 
