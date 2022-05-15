cf0c4b64a43518cd0f981df3dede4f9e96fcd05b
Taras Slovak <slovak.taras@gmail.com>
2022-05-15 15:59:39 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
-import { Routes } from './components/Routes';
+import { Routing } from './components/Routing';
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -11,7 +11,7 @@ function App() {
     <div className={darkTheme ? 'dark' : ''}>
       <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
         <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
-        <Routes />
+        <Routing />
         <Footer />
       </div>
     </div>
--- src/components/Routes.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Routes() {
-  return (
-    <div>Routes</div>
-  );
-}
