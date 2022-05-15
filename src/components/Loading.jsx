ad7b2ea47b99266f2248d59f6f58ece92c0eaabf
Taras Slovak <slovak.taras@gmail.com>
2022-05-15 17:50:39 +0300

    fix

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Rings from 'react-loader-spinner';
+import { Rings } from 'react-loader-spinner';
 
 export function Loading() {
   return (
     <div className="flex justify-center items-center ">
-      <Rings type="Puff" color="#00BFFF" height={550} width={80} />
+      <Rings color="#00BFFF" height={550} width={80} />
     </div>
   );
 }
