cfea23e18ee9e29e38b6fa3838afc1cc431f9321
Taras Slovak <slovak.taras@gmail.com>
2022-05-15 18:52:21 +0300

    fix bugs

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -13,8 +13,12 @@ export function Links() {
     <div className="flex sm:justify-around justify-between items-center mt-4">
       {links.map(({ url, text }) => (
         <NavLink
+          key={text}
           to={url}
-          activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
+          // className="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
+          className={({ isActive }) => (isActive
+            ? 'text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2'
+            : '')}
         >{text}
         </NavLink>
       ))}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ export function Navbar({ setDarkTheme, darkTheme }) {
           type="button"
           onClick={() => setDarkTheme(!darkTheme)}
           className="text-xl dark:bg-gray-50 dark:text-gray-900
-          bg-white border rounded-full px-2 py-1 hover:shadow-lg"
+          bg-white border rounded-full pl-2 pr-5 py-1 hover:shadow-lg"
         >{darkTheme ? '💡 Light' : '🌙 Dark'}
         </button>
       </div>
