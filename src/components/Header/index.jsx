96801ff1b8bd3dcce4ff4242fbf567c1436afa09
Taras Slovak <slovak.taras@gmail.com>
2022-07-05 09:49:25 +0300

    The blog layout has been created without functionality

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+
+export const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth='lg'>
+        <div className={styles.inner}>
+          <a className={styles.logo} href='/'>
+            <div>TARAS BLOG</div>
+          </a>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <a href='/posts/create'>
+                  <Button variant='contained'>Write an article</Button>
+                </a>
+                <Button
+                  onClick={onClickLogout}
+                  variant='contained'
+                  color='error'>
+                  Log out
+                </Button>
+              </>
+            ) : (
+              <>
+                <a href='/login'>
+                  <Button variant='outlined'>Sign In</Button>
+                </a>
+                <a href='/register'>
+                  <Button variant='contained'>Create an account</Button>
+                </a>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
